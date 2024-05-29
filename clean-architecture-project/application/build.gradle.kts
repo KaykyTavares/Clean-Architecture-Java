d4bce131820853a45ee5da03297cef6ad61efdbb
KaykyTavares <kayky.programmer@gmail.com>
2024-05-29 15:52:05 -0300

    feat: add core and useCase module as application dependencie

--- clean-architecture-project/application/build.gradle.kts
+++ clean-architecture-project/application/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation(project(path = ":core"))
+    implementation(project(path = ":useCase"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
