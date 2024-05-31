d867e4fb2293847dfe64b38a4c22582bb272aa47
KaykyTavares <kayky.programmer@gmail.com>
2024-05-31 18:50:41 -0300

    fix(core): Exception -> create EmailException

---
+++ clean-architecture-project/core/src/main/java/br/com/course/core/exception/EmailException.java
@@ -0,0 +1,10 @@
+package br.com.course.core.exception;
+
+public class EmailException extends Exception {
+    private String code;
+
+    public EmailException(String message, String code) {
+        super(message);
+        this.code = code;
+    }
+}
