edd0493d00fc3afc3f5a5ed6ceda47b6e676b4da
KaykyTavares <kayky.programmer@gmail.com>
2024-05-31 18:48:02 -0300

    fix(useCase): change CreateUserUseCase method params and thrws exceptions

--- clean-architecture-project/useCase/src/main/java/br/com/course/useCase/CreateUserUseCase.java
+++ clean-architecture-project/useCase/src/main/java/br/com/course/useCase/CreateUserUseCase.java
@@ -1,7 +1,10 @@
 package br.com.course.useCase;
 
 import br.com.course.core.domain.User;
+import br.com.course.core.exception.EmailException;
+import br.com.course.core.exception.TaxNumberException;
+import br.com.course.core.exception.TransectionPinException;
 
 public interface CreateUserUseCase {
-    void create(User user);
+    void create(User user, String pin) throws TaxNumberException, EmailException, TransectionPinException;
 }
