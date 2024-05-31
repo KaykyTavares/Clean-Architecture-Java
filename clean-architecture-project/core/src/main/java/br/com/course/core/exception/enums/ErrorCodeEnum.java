14914a7622cb6e88ece596a5d5d6ef685e58e17c
KaykyTavares <kayky.programmer@gmail.com>
2024-05-31 14:23:00 -0300

    feat(core): exception -> create ErrorCodeEnum

---
+++ clean-architecture-project/core/src/main/java/br/com/course/core/exception/enums/ErrorCodeEnum.java
@@ -0,0 +1,32 @@
+package br.com.course.core.exception.enums;
+
+public enum ErrorCodeEnum {
+    ON0001("Invalid Tax number", "ON-0001"),
+    TR0001("Shop User doesn't have transfer function available", "TR-0001"),
+    TR0002("Unavailable balance", "TR-0002"),
+    TRP0001("Invalid Pin", "TRP-0001")
+    ;
+    private String message;
+    private String code;
+
+    ErrorCodeEnum(String message, String code) {
+        this.message = message;
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
