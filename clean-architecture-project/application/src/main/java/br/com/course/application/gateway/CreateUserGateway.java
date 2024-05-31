fcdf4155fb5f60c12b39fb42d7de53c2980dabd1
KaykyTavares <kayky.programmer@gmail.com>
2024-05-31 18:51:18 -0300

    fix(application): Gateway -> create CreateUserGateway

---
+++ clean-architecture-project/application/src/main/java/br/com/course/application/gateway/CreateUserGateway.java
@@ -0,0 +1,9 @@
+package br.com.course.application.gateway;
+
+import br.com.course.core.domain.User;
+
+public class CreateUserGateway {
+    public User create(User user) {
+        return user;
+    }
+}
