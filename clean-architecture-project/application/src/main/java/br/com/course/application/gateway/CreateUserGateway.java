f6436ab202d90119356ea8a5cda17ad44afc612b
KaykyTavares <kayky.programmer@gmail.com>
2024-06-04 09:49:45 -0300

    fix(application): Gateway -> change CreateUserGateway class to an interface

--- clean-architecture-project/application/src/main/java/br/com/course/application/gateway/CreateUserGateway.java
+++ clean-architecture-project/application/src/main/java/br/com/course/application/gateway/CreateUserGateway.java
@@ -2,8 +2,6 @@ package br.com.course.application.gateway;
 
 import br.com.course.core.domain.User;
 
-public class CreateUserGateway {
-    public User create(User user) {
-        return user;
-    }
+public interface CreateUserGateway {
+    User create(User user);
 }
