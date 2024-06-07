8e676e5fadec33f03cb390bda90031437c1c388f
KaykyTavares <kayky.programmer@gmail.com>
2024-06-07 16:09:43 -0300

    feat(core): exception -> add new code exceptions to enum

--- clean-architecture-project/core/src/main/java/br/com/course/core/exception/enums/ErrorCodeEnum.java
+++ clean-architecture-project/core/src/main/java/br/com/course/core/exception/enums/ErrorCodeEnum.java
@@ -5,10 +5,19 @@ public enum ErrorCodeEnum {
     ON0002("Unavailable Tax number", "ON-0002"),
     ON0003("Unavailable email", "ON-0003"),
     ON0004("An error occurred on create user", "ON-0004"),
+
     TR0001("Shop User doesn't have transfer function available", "TR-0001"),
     TR0002("Unavailable balance", "TR-0002"),
     TR0003("An error occurred on doing the transfer", "TR-0003"),
-    TRP0001("Invalid Pin", "TRP-0001")
+    TR0004("Unauthorized transfer", "TR-0004"),
+
+    TRP0001("Invalid Pin", "TRP-0001"),
+
+    WA0001("Wallet not found", "WA-0001"),
+
+    NO0001("An error occurred on user's notification", "NO-0001"),
+
+    ATH0001("An error occurred on authenticate user", "ATH-0001")
     ;
     private String message;
     private String code;
